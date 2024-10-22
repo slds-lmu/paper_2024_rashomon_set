641341c554ae35ed880800b1728c68839f407769
mb706 <mb706@users.noreply.github.com>
2024-10-22 12:39:15 +0200

    repair compas task

--- R/assets/tasks.R
+++ R/assets/tasks.R
@@ -4,7 +4,9 @@ task.bh$select(selector_invert(selector_cardinality_greater_than(2))(task.bh))
 
 
 task.gc <- tsk("german_credit")
+
 task.cs <- tsk("compas")
+task.cs$select(selector_invert(selector_name("is_recid"))(task.cs))
+
 task.bs <- tsk("bike_sharing")
 task.bs$select(selector_invert(selector_type("character"))(task.bs))
-
