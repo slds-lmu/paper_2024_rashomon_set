180abf8f1fe5977adb8efa23b5d333aaef1bbb4f
mb706 <mlr.developer@mb706.com>
2025-05-13 13:36:55 +0200

    status

---
+++ space_disjoiner_demo.R
@@ -0,0 +1,29 @@
+
+# render with knitr::spin("space_disjoiner_demo.R", knit = TRUE, report = FALSE)
+
+parset <- ps(
+  x = p_dbl(0, 1),
+  y = p_dbl(0, 1),
+  a = p_fct(c("a", "b", "c")),
+  b = p_lgl(depends = a == "b"),
+  c = p_lgl(depends = a %in% c("b", "c") && b == TRUE),
+  d = p_fct(c("x", "y"), depends = a == "c"),
+  only.in.a.ab = p_dbl(0, 1, depends = a %in% c("a", "b")),
+  only.in.c.true = p_dbl(0, 1, depends = c == TRUE)
+)
+
+parset
+
+sdx <- SpaceDisjoiner$new(parset)
+
+sdx$param.set
+
+sdx$subspaces
+
+grid <- generate_design_grid(parset, 2)
+
+grid$data
+
+sdx$disjoinTable(grid$data)
+
+sdx$disjoinTable(grid$data[c(7, 5, 4, 1)])
